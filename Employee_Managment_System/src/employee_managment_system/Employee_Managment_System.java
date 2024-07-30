4f1fbdf816ec8078afceb4a47bffc3358adb7f0f
hayfa.jundi <haifazaqwan@gmail.com>
2024-07-30 20:18:21 +0300

    EMS

---
+++ Employee_Managment_System/src/employee_managment_system/Employee_Managment_System.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package employee_managment_system;
+
+/**
+ *
+ * @author Haifa Aljundi
+ */
+public class Employee_Managment_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
